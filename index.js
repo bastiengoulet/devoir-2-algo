2d83404de925c0aa23353a4a20dd8b73e61e6a16
Bastien Goulet <bastien.goulet27@outlook.com>
2022-02-17 15:31:57 -0500

    Application console permettant la conversion d'un fichier encodé en UTF-32 vers un fichier en UTF-8

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
+const path = require("path");
 const codec = require("./src/encodageUtf32VersUtf8.js");
+const args = process.argv.slice(2)
 
-/*var buff = Buffer.alloc(4);
-buff[0] = 0x01;
-buff[1] = 0xF6;
-buff[2] = 0x01;
-buff[3] = 0x00;
+if (args.length == 2)
+{
+    var nomFichier = path.parse(args[1]).name;
+    var fichierDest = "./output/" + nomFichier + ".utf8";
 
-var utf8Char = codec.convertirCharUnicode32VersUtf8(buff);
-console.log(utf8Char);*/
+    if (args[0] == "+")
+    {
+        codec.convertirUnicode32VersUtf8(args[1], fichierDest);
+    }
+    else if (args[0] == "-")
+    {
 
-codec.convertirUnicode32VersUtf8("./ressources/texte3.unicode32", "./output/texte3.utf8");
+    }
+    else
+    {
+        console.log("Le premier argument doit être {+} pour convertir UTF-32 à UTF-8, ou {-} pour convertir UTF-8 à UTF-32.");
+    }
+}
+else
+{
+    console.log("Veuillez fournir 2 arguments: node index.js {+/-} {Nom du fichier source}");
+}
